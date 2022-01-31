02c6f7ca6bbb0723d6c9a1cb43421b3fd484069d
Roman <r.reset@gmail.com>
2022-01-31 21:24:58 +0200

    android dice roller

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import java.util.Random
+
+class MainActivity : AppCompatActivity() {
+    lateinit var diceImageView: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById<Button>(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+
+        diceImageView = findViewById(R.id.dice)
+    }
+
+    private fun rollDice() {
+        val currentDice = when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImageView.setImageResource(currentDice)
+    }
+}
